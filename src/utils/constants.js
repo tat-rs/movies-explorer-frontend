30e8dc382ae7e47bacd65fd30bac2de6fa840e69
Tatiana Roslyakova <tat-rs@yandex.ru>
2022-11-12 18:10:44 +0300

    fix api url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
-export const URL_MOVIE_API = 'https://api.nomoreparties.co/beatfilm-movies';
+export const URL_MOVIE_API = 'https://movies-explorer-api-six.vercel.app';
 
 export const URL_MAIN_API = 'https://movie-explorer-api22.herokuapp.com';
 
